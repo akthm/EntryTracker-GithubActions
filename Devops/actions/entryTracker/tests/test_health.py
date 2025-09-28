cdab7ac5ee705f8f10f90ce506d18901b7a28e35
Akthm Daas <akthm.daas@gmail.com>
2025-09-28 20:43:33 +0300

    compose condition mysql

--- Devops/actions/entryTracker/tests/test_health.py
+++
@@ -1,9 +0,0 @@
-import os, sys
-sys.path.append(os.path.dirname(os.path.dirname(__file__)))
-
-import app  
-
-def test_health():
-    client = app.app.test_client()
-    resp = client.get("/health")
-    assert resp.status_code == 200
